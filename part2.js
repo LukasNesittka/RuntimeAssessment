3bc9c5de57e73d7b850f67646e523277e7f8e7ce
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-02-18 10:17:25 -0600

    extraCredit

--- part2.js
+++ part2.js
@@ -22,12 +22,16 @@ console.log(addToZero([1, 2, 3]))
 console.log(addToZero([1, 2, 3, -2]))
 console.log('\n')
 
+//The space complexity of this function is O(n^2) because the function loops twice through an array of undefined length, making its runtime grow exponentially as the array grows.
+
 const hasUniqueChars = (str) => {
     return new Set(str).size == str.length;
 }
 console.log(hasUniqueChars("Monday"))
 console.log(hasUniqueChars("Moonday"))
 console.log('\n')
+
+//The space complexity of this function is O(1). It is a simple truthy request, and doesn't change runtime based on the length of the string, as it is merely checking for the first of each value.
 
 const isPangram = (str) => {
     const hasAlpha = /([a-z])(?!.*\1)/g;
@@ -36,6 +40,8 @@ const isPangram = (str) => {
 console.log(isPangram("The quick brown fox jumps over the lazy dog!"))
 console.log(isPangram("I like cats, but not mice"))
 console.log('\n')
+
+//The space complexity of this function is O(1). Once again, it is a simply truthy statement, and there are only so many letters in the alphabet to check against.
 
 const findLongestWord = (arr) => {
     let longestWord = 0
@@ -48,3 +54,5 @@ const findLongestWord = (arr) => {
     return longestWord
 }
 console.log(findLongestWord(["hi", "hello"]))
+
+//The space complexity of this function is O(n). Since we are relying on an array, the runtime increases as the array grows, linearly.
